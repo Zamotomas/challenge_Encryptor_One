ad47f88e6a110a859023b6d7773a53cf75d73824
P.F <alanbastian.fuentes@gmail.com>
2022-08-06 04:51:25 -0400

    Encryption and decryption functions are enabled

--- scripts/index.js
+++ scripts/index.js
@@ -1,54 +1,61 @@
 
-//Importa datos 
-function importarMensaje(){
+
+//1-Importa datos 
+
+function importarMensaje() {
+  console.log(document.getElementById("areaText").value.toLowerCase());
   return document.getElementById("areaText").value.toLowerCase();
 }
 
 
-//Evalua y prepara los datos
-function revisar(text){
- let mensaje = text;
- mensaje = text.replace(/[^a-zA-Z ]/g, "");
- if (mensaje != text){
-  alert("Se eliminaron los caracteres invalidos de su mensaje")
-  return mensaje;
-} else {
-  return mensaje;
-}
-}
-
-
-
-//Modifica los mensaje 
-
-function encriptar(mensaje){
-  let encriptado ="";
-  encriptado = mensaje.replace("e","enter");
-  encriptado = encriptado.replace("i","imes");
-  encriptado = encriptado.replace("a","ai");
-  encriptado = encriptado.replace("o","ober");
-  encriptado = encriptado.replace("u","ufat");
-  return encriptado;
-}
-
-function desencriptar(mensaje){
-  let desEncriptado ="";
-  desEncriptado = mensaje.replace("enter","e");
-  desEncriptado = desEncriptado.replace("imes","i");
-  desEncriptado = desEncriptado.replace("ai","a");
-  desEncriptado = desEncriptado.replace("ober","o");
-  desEncriptado = desEncriptado.replace("ufat","u");
-  return desEncriptado;
+//2-Evalua y prepara los datos
+function revisar(text) {
+  let mensaje = text;
+  mensaje = text.replace(/[^a-zA-Z ]/g, "");
+  if (mensaje != text) {
+    alert("Se eliminaron los caracteres invalidos de su mensaje")
+    return mensaje;
+  } else {
+    return mensaje;
+  }
 }
 
 
-//Prepara los campos de datos
+//3-Prepara y elimina el campo de imagen en html
 
 
-//Muestra los datos
-  
+//4-Muestra los datos en html
+function mostrar(mensaje) {
+ document.querySelector(".mensajeFinal").innerHTML = mensaje;
+}
+
+
+
+//5- Encripta el mensaje
+function encriptar() {
+  let mensaje = revisar(importarMensaje());
+  mensaje = mensaje.replace("e", "enter");
+  mensaje = mensaje.replace("i", "imes");
+  mensaje = mensaje.replace("a", "ai");
+  mensaje = mensaje.replace("o", "ober");
+  mensaje = mensaje.replace("u", "ufat");
+  mostrar(mensaje)
+}
+
+//6- Desencripta el mensaje
+function desencriptar() {
+  let mensaje = revisar(importarMensaje());
+  mensaje = mensaje.replace("enter", "e");
+  mensaje = mensaje.replace("imes", "i");
+  mensaje = mensaje.replace("ai", "a");
+  mensaje = mensaje.replace("ober", "o");
+  mensaje = mensaje.replace("ufat", "u");
+  mostrar(mensaje)
+}
+
 
 
 //Activacion de la Botonera
-document.getElementById("desencriptar").onclick = desencriptar
 document.getElementById("encriptar").onclick = encriptar
+document.getElementById("desencriptar").onclick = desencriptar
+
